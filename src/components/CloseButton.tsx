c6b8ac7d13a59ae2aafa948bccefa1898ea8bb9c
Adriano Domingues <54463264+programadormovel@users.noreply.github.com>
2022-05-05 10:23:56 -0300

    first commit

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,11 @@
+import { Popover } from "@headlessui/react";
+import { X } from "phosphor-react";
+
+export function CloseButton() {
+    return (
+        <Popover.Button className="top-5 right-5 absolut text-zinc-400 hover:text-zinc-100" 
+            title="Fechar Formulário">
+                <X weight="bold" className="w-4 h4" />
+            </Popover.Button>
+    )
+}
---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,72 @@
+import bugImageUrl from "../../assets/problem.png";
+import ideaImageUrl from "../../assets/idea.png";
+import otherImageUrl from "../../assets/outro.png";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedBackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedBackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Idea",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: otherImageUrl,
+      alt: "Imagem de uma nuvem",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+    setFeedbackSent(false);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback} />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer>
+        <a
+          className="underline underline-offset-2"
+          href="https://www.mongreltech.com.br"
+        >
+          MongrelTech Solutions
+        </a>
+      </footer>
+    </div>
+  );
+}
